0117a14e99ab31cdffb5df7e44923c1b126c017c
Steve Suarez <suarezsteve21@gmail.com>
2020-05-05 20:55:44 -0500

    Creating system navigation

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './app/Navigations/Navigation'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola mundo!</Text>
-    </View>
-  );
+    <Navigation />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,12 +1,15 @@
-import React from 'react-native'
+import React from 'react'
 // Estamos importanto el componente de Navigation container ya que el sistema de navegacion tiene que estar envuelto en este componente para que funcione
 import { NavigationContainer } from '@react-navigation/native'
 // Improtamos una función para crear nuestro menú tam
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 // Importamos una de mis screens
-import Home from './../screens/Home'
-
+import Home from '../screens/Home'
+// Importo mi screen Recipes
+import Recipes from '../screens/Recipes'
+import EventsCalendar from '../screens/EventsCalendar'
+import Account from '../screens/Account'
 // Crearemos el componente
 const Tab = createBottomTabNavigator();
 
@@ -15,9 +18,26 @@ export default function Navigation() {
     <NavigationContainer>
       <Tab.Navigator>
         {/* Aquí deberia ir nuestra screen de acuerdo al tab, primero le daremos un nombre a la screen, en minuscula y sin espacios */}
-        <Tab.Screen njame="home" />
+        {/* Le decimos el componente que tiene que renderizar cuando se entre a esta screen */}
+        <Tab.Screen
+          name="home"
+          component={Home}
+          options={{ title: "Home" }} />
+        <Tab.Screen
+          name="recipes"
+          component={Recipes}
+          options={{ title: "Recetas" }} />
+        <Tab.Screen
+          name="events"
+          component={EventsCalendar}
+          options={{ title: "Calendario" }} />
+        <Tab.Screen
+          name="account"
+          component={Account}
+          options={{ title: "Cuenta" }} />
+
       </Tab.Navigator>
     </NavigationContainer>
 
-  )
+  );
 }
